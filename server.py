ca4ec50b43104fc250a6e3c9d2a32e749e7e08bb
James Arthur <thruflo@gmail.com>
2015-05-28 16:32:38 +0100

    server: configurable keep alive, headers and max delay.

--- server.py
+++ server.py
@@ -1,18 +1,28 @@
 
 import gevent
+import os
 
-HEADERS = [
-    ("Connection", "Keep-Alive"),
-    ("Content-Type", "text/plain"),
-]
+settings = {
+    'max_delay': int(os.environ.get('MAX_DELAY', 120)),
+    'use_empty_strings': bool(os.environ.get('USE_EMPTY_STRINGS', False)),
+}
 
 def stream():
-    i = 2
-    while True:
-        yield '{0}\r\n'.format(i)
-        gevent.sleep(i)
+    i = 0
+    message = '' if settings['use_empty_strings'] else '{0}\r\n'
+    while i < settings['max_delay']:
         i = i + 2
+        gevent.sleep(i)
+        yield message.format(i)
+    yield '\r\n'
+    yield 'Done!'
 
 def app(environ, start_response):
-    start_response("200 OK", headers=HEADERS)
+    headers = [
+        ("Content-Type", "text/plain"),
+    ]
+    if environ.get('HTTP_CONNECTION') == 'keep-alive':
+        headers.append(("Connection", "Keep-Alive"))
+    print headers
+    start_response("200 OK", headers=headers)
     return stream()
