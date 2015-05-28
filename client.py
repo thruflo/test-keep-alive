f586d985858c6712c79c18b54548a22cbc6f0775
James Arthur <thruflo@gmail.com>
2015-05-28 16:04:45 +0100

    client: refactor to support --stream / --no-stream.

--- client.py
+++ client.py
@@ -1,14 +1,32 @@
-import requests
-import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def consume(r):
-    for line in r.iter_lines():
-        print line
+import argparse
+import requests
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='https://test-keep-alive.herokuapp.com')
+    parser.add_argument('--stream', dest='should_stream', action='store_true')
+    parser.add_argument('--no-stream', dest='should_stream', action='store_false')
+    parser.set_defaults(should_stream=True)
+    return parser.parse_args()
+
+def main(host, should_stream):
+    if should_stream:
+        r = requests.get(host, stream=True)
+        try:
+            for line in r.iter_lines():
+                print line
+        finally:
+            r.close()
+    else:
+        r = requests.get(args.host)
+        print r.text
 
 if __name__ == '__main__':
-    r = requests.get('https://test-keep-alive.herokuapp.com', stream=True)
+    args = parse_args()
     try:
-        consume(r)
-    except (KeyboardInterrupt, StopIteration):
+        main(args.host, args.should_stream)
+    except KeyboardInterrupt:
         pass
-    r.close()
