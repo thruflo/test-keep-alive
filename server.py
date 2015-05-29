3a8dc7a67500054ac84114f552d4e627beed1656
James Arthur <thruflo@gmail.com>
2015-05-29 12:13:24 +0100

    server: switch to null bytes.

--- server.py
+++ server.py
@@ -6,20 +6,20 @@ import time
 
 settings = {
     'interval': int(os.environ.get('INTERVAL', 5)),
-    'timeout': int(os.environ.get('TIMEOUT', 120)),
+    'timeout': int(os.environ.get('TIMEOUT', 65)),
     'use_empty_strings': bool(os.environ.get('USE_EMPTY_STRINGS', False)),
 }
 
 def stream():
     delay = settings['interval']
     deadline = time.time() + settings['timeout']
-    message = '' if settings['use_empty_strings'] else '{0}\r\n'
+    message = '' if settings['use_empty_strings'] else '\0'
     while True:
         if time.time() > deadline:
             break
-        msg = message.format(delay)
-        print msg
-        yield msg
+        # msg = message.format(delay)
+        print message
+        yield message
         gevent.sleep(delay)
     payload = json.dumps({'status': u'Done'})
     print payload
